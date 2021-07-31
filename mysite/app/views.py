a8a7e94a2ee487e53f98f567ee79d33490e6eea0
jeroux <jeremylipszyc@gmail.com>
2021-07-31 17:49:53 +0200

    mise en fonctionnement du modèle

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -9,6 +9,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from tensorflow import keras
 import tensorflow as tf
+from users.forms import UploadFileForm
+
+from django.shortcuts import render
+from django.http import JsonResponse
+import base64
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.conf import settings 
+from tensorflow.python.keras.backend import set_session
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.imagenet_utils import decode_predictions
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.applications import vgg16
+import datetime
+import traceback
 
 def index(request):
     """ or return render(request, 'index.html') """
@@ -62,16 +79,75 @@ def give_help(request):
     return render(request, 'give_help.html') 
 
 def photo(request):
-    
-    return render(request, 'photo.html')
+    if  request.method == "POST":
+        f=request.FILES['sentFile'] # here you get the files needed
+        response = {}
+        file_name = "pic.jpg"
+        file_name_2 = default_storage.save(file_name, f)
+        file_url = default_storage.url(file_name_2)
+        original = load_img("media/pic.jpg", target_size=(224, 224))
+        numpy_image = img_to_array(original)
+        
+
+        image_batch = np.expand_dims(numpy_image, axis=0)
+        # prepare the image for the VGG model
+        processed_image = vgg16.preprocess_input(image_batch.copy())
+        
+        # get the predicted probabilities for each class
+        with settings.GRAPH1.as_default():
+            set_session(settings.SESS)
+            VGG_MODEL = vgg16.VGG16(weights="imagenet")
+            predictions=VGG_MODEL.predict(processed_image)
+       
+        label = decode_predictions(predictions)
+        label = list(label)[0]
+        response['name'] = str(label)
+        return render(request,'photo.html',response)
+    else:
+        return render(request,'photo.html')
 
 def result_photo(request):
-    print("debut result view")
-    image = request.POST["photo"]
-    '''
-    image_decoded = tf.image.decode_jpeg(img, channels=3)
-    image = tf.cast(image_decoded, tf.float32)'''
-    image_resized = tf.image.resize_images(image, [96, 96])
+    if  request.method == "POST":
+        f=request.FILES['sentFile'] # here you get the files needed
+        response = {}
+        file_name = "pic.jpg"
+        file_name_2 = default_storage.save(file_name, f)
+        file_url = default_storage.url(file_name_2)
+        original = load_img(file_url, target_size=(224, 224))
+        numpy_image = img_to_array(original)
+        
+
+        image_batch = np.expand_dims(numpy_image, axis=0)
+        # prepare the image for the VGG model
+        processed_image = vgg16.preprocess_input(image_batch.copy())
+        
+        # get the predicted probabilities for each class
+        with settings.GRAPH1.as_default():
+            set_session(settings.SESS)
+            predictions=settings.VGG_MODEL.predict(processed_image)
+       
+        label = decode_predictions(predictions)
+        label = list(label)[0]
+        response['name'] = str(label)
+        return render(request,'homepage.html',response)
+    else:
+        return render(request,'homepage.html')
+    '''print("debut result view")
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        
+        f = request.FILES["file"]
+        with open('photo.jpg', 'wb+') as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+            
+    else:
+        form = UploadFileForm()
+        image = "coucou"
+    
+    image = 
+    
+    image_resized = tf.image.resize(image, [96, 96])
     print("apres resize")
     model = keras.models.load_model('Model MobileNet') 
     print("avant prediction")
@@ -80,7 +156,8 @@ def result_photo(request):
 
     print("prediction", prediction)
 
-    return render(request, 'photo.html')
+    return render(request, 'photo.html')'''
+    
 
 def shipping_list(request):
     liste_shipping = Shipping.objects.filter(state='pending').order_by('sending_date')
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,3 +131,35 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL= "login"
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "index"
+
+import keras
+import numpy as np
+from keras import backend as K
+import tensorflow as tf
+from tensorflow.python.keras.backend import set_session
+from keras.applications import vgg16
+
+
+def get_session():
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    return tf.compat.v1.Session(config=config)
+
+set_session(get_session())
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+SESS = tf.compat.v1.Session(config=config)
+print("model loading")
+GRAPH1 = tf.compat.v1.get_default_graph()
+
+set_session(SESS)
+# Load the VGG model
+VGG_MODEL = vgg16.VGG16(weights="imagenet")
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
